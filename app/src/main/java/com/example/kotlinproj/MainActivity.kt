6552fc8aacaf847d1522c96d531c51c58c5e2aa8
bao <baopzzoo@gmail.com>
2021-08-12 10:08:34 +0700

    commit 1

--- app/src/main/java/com/example/kotlinproj/MainActivity.kt
+++ app/src/main/java/com/example/kotlinproj/MainActivity.kt
@@ -4,8 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private val a="ddd"
+
+    private val b= "da"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
+
+
 }
